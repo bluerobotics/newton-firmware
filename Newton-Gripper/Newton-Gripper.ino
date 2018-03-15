fb29e15edaf0c4578c69be9407cd3a5112a9550a
dheideman <dheideman@socal.rr.com>
2018-03-14 19:36:40 -0700

    Prettify code

--- Newton-Gripper/Newton-Gripper.ino
+++ Newton-Gripper/Newton-Gripper.ino
@@ -34,88 +34,25 @@ THE SOFTWARE.
 -------------------------------*/
 
 #include <DiscreteFilter.h>
+#include "Newton-Gripper.h"
 
-// HARDWARE PIN DEFINITIONS
-#define PWM_IN      8   // INT0
-#define LED         4
-#define CURRENT_IN  1   // from A4955 AIOUT
-#define SLEEPN      3
-#define OUT1        6   // OCR1A
-#define OUT2        5   // OCR1B
-#define VOLTAGE_IN  0   // from voltage divider
-
-// CURRENT SENSOR
-#define R_SENSE     0.005f            // ohms
-
-// VOLTAGE SENSOR
-#define V_IN        3.3f              // volts
-#define V_SENSE_DIV 0.155f            // v_sense voltage divider (3.3/(3.3+18))
-
-// PWM INPUT TIMEOUT
-#define INPUT_TOUT  0.050f            // s
-
-// PWM READ DEFINITIONS
-#define PWM_FREQ    50                // Hz
-#define PERIOD      1000000/PWM_FREQ  // us
-#define PWM_MAX     1900              // us
-#define PWM_MIN     1100              // us
-#define PWM_NEUTRAL 1500              // us
-#define HALF_RANGE  400               // us
-#define INPUT_MAX   2500              // us
-#define INPUT_MIN   500               // us
-#define INPUT_DZ    30                // us
-#define PRESCALE    1                 // must match TCCR1B settings
-#define CNT_PER_US  (F_CPU/PRESCALE/1000000L) // timer counts
-#define MAX_COUNT   0x0FFF          // sets output PWM frequency (~1 kHz)
-
-// PWM OUTPUT CHARACTERISTICS
-#define OUTPUT_DZ   0.60f             // speed 0.0~1.0 (deadzone)
-#define MAX_DUTY    1.00f             // sets maximum output @ reference voltage
-
-// MAX/MIN DUTY PARAMETERS
-#define MAX_DUTY_A0 1.55f             // max duty curve constant term
-#define MAX_DUTY_A1 -0.063f           // max duty curve linear term
-#define MAX_DUTY_A2 0.0011f           // max duty curve quadratic term
-#define MIN_DUTY_A0 0.87f             // min duty curve constant term
-#define MIN_DUTY_A1 -0.027f           // min duty curve linear term
-#define MIN_DUTY_A2 0.00048f          // min duty curve quadratic term
-
-// FILTER PARAMETERS
-#define FILTER_DT   0.050f            // s
-#define TAUP_OUT    0.500f            // sets lp filtering of lead-lag (s)
-#define TAUN_OUT    TAUP_OUT/1.5f     // sets starting gain of lead-lag (s)
-#define TAU_CURRENT 0.03000f          // s
-#define CURRENT_DT  0.00010f          // s
-
-// STALL PARAMETERS
-#define I_STALL_A0  0.00514f          // stall current const. coefficient term
-#define I_STALL_A1  0.00294f          // stall current linear coefficient term
-#define I_STALL_B1  4.69f             // stall current exponential term
-#define I_LIMIT_IN  0.50f             // fraction of stall current (closing)
-#define I_LIMIT_OUT 0.70f             // fraction of stall current (opening)
-
-// CURRENT/VOLTAGE PARAMETERS
-#define V_BASE      12.0f             // voltage at which measurements were made
-#define I_RATIO_A1  0.0833f           // current ratio linear term
-
-// Custom Enumerated Types
-enum dir_t {
-  NONE = 0,
-  FORWARD,
-  REVERSE
-};
-
+// Global Variables
 uint32_t lastpulsetime        = 0;
-uint32_t updatefilterruntime  = 0;
-uint32_t updatecurrentruntime = 0;
+uint32_t speedfilterruntime   = 0;
+uint32_t currentfilterruntime = 0;
 
 int16_t  pulsein  = PWM_NEUTRAL;
 dir_t    limit    = NONE;
 dir_t    lastdir  = NONE;
 float    velocity = 0.0f;
-DiscreteFilter outputfilter;
+DiscreteFilter speedfilter;
 DiscreteFilter currentfilter;
 
+
+
+///////////
+// SETUP //
+///////////
 
 void setup() {
   // Set up pin modes
@@ -134,7 +71,7 @@ void setup() {
   initializePWMOutput();
 
   // Initialize output low-pass filter
-  outputfilter.createLeadLagCompensator(FILTER_DT, TAUP_OUT, TAUN_OUT);
+  speedfilter.createLeadLagCompensator(FILTER_DT, TAUP_OUT, TAUN_OUT);
 
   // Initialize current input low-pass filter
   currentfilter.createFirstOrderLowPassFilter(CURRENT_DT, TAU_CURRENT);
@@ -143,121 +80,183 @@ void setup() {
   // Wake up A4955
   digitalWrite(SLEEPN, HIGH);
 
-  // Turn off LED (for testing)
+  // Turn off LED
   digitalWrite(LED, LOW);
 }
+
+
+//////////
+// LOOP //
+//////////
 
 void loop() {
   // Make sure we're still receiving PWM inputs
   if ( (millis() - lastpulsetime)/1000.0f > INPUT_TOUT ) {
     // If it has been too long since the last input, shut off motor
     pulsein = PWM_NEUTRAL;
-
-    // Force filters to update immediately
-    updatefilterruntime = 0;
+    lastpulsetime = millis();
   } // end pwm input check
 
-  // Run filters at specified interval
-  if ( millis() > updatefilterruntime ) {
+  // Run speed filter at specified interval
+  if ( millis() > speedfilterruntime ) {
     // Set next filter runtime
-    updatefilterruntime = millis() + FILTER_DT*1000;
+    speedfilterruntime = millis() + FILTER_DT*1000;
 
-    // Declare local variables
-    float rawvelocity;
-    dir_t direction;
-    float voltage = readVoltage();
-    float maxduty = min(maxDuty(voltage),MAX_DUTY);
-    float minduty = max(minDuty(voltage),0.0f);
-
-    // Reject signals that are way off (i.e. const. 0 V, const. +5 V, noise)
-    if ( pulsein >= INPUT_MIN && pulsein <= INPUT_MAX ) {
-      // Remove neutral PWM bias & clamp to [-HALF_RANGE, HALF_RANGE]
-      int16_t pw = constrain(pulsein - PWM_NEUTRAL, -HALF_RANGE, HALF_RANGE);
-
-      // Reject anything inside input deadzone
-      if ( pw > INPUT_DZ ) {
-        // Map valid PWM signals to (0.0 to 1.0]
-        rawvelocity = (float)(pw - INPUT_DZ)/(HALF_RANGE - INPUT_DZ)
-                      *(maxduty - minduty) + minduty;
-      } else if ( pw < -INPUT_DZ ) {
-        // Map valid PWM signals to [-1.0 to 0.0)
-        rawvelocity = (float)(pw + INPUT_DZ)/(HALF_RANGE - INPUT_DZ)
-                      *(maxduty - minduty) - minduty;
-      } else {
-        // Stop motor if input is within input deadzone
-        rawvelocity = 0.0f;
-      }
-    } else {
-      // Stop motor if input is invalid
-      rawvelocity = 0.0f;
-    }
-
-    // Filter velocity
-    velocity = constrain(outputfilter.step(rawvelocity), -maxduty, maxduty);
-
-    // Figure out the current direction of travel
-    if (velocity > minduty) {
-        direction = FORWARD;
-    } else if (velocity < -minduty) {
-        direction = REVERSE;
-    } else {
-        direction = NONE;
-    }
-
-    // Clear current filter if we change directions
-    if (direction != lastdir) {
-      currentfilter.clear();
-    }
-
-    // Current sensor resolution: ~65 mA
-    if (direction == FORWARD && currentfilter.getLastOutput() > I_LIMIT_OUT) {
-      limit = direction;
-      digitalWrite(LED, LOW);
-    } else if (direction == REVERSE && currentfilter.getLastOutput() > I_LIMIT_IN) {
-      limit = direction;
-      digitalWrite(LED, LOW);
-    } else if ( /*direction != NONE &&*/ limit != NONE && direction != limit ) {
-      // We're going the opposite direction from the limit, so clear limit
-      limit = NONE;
-      digitalWrite(LED, HIGH);
-    }
-
-    // Set output PWM timers
-    if ( direction == FORWARD && limit != FORWARD) {
-      OCR1A = abs(velocity)*MAX_COUNT;
-      OCR1B = 0;
-    } else if ( direction == REVERSE && limit != REVERSE) {
-      OCR1A = 0;
-      OCR1B = abs(velocity)*MAX_COUNT;
-    } else {
-      OCR1A = MAX_COUNT;
-      OCR1B = MAX_COUNT;
-    }
-
-    // Save last direction
-    lastdir = direction;
+    // Update input filter
+    runSpeedFilter();
   } // end run filters
 
-  // Current lp filter
-  if ( micros() > updatecurrentruntime ) {
+  // Run current lp filter at specified interval
+  if ( micros() > currentfilterruntime ) {
     // Set next current lp filter runtime
-    updatecurrentruntime = micros() + CURRENT_DT*1000000;
+    currentfilterruntime = micros() + CURRENT_DT*1000000;
+
+    // Update current filter
     currentfilter.step(readCurrent()/stallCurrent(velocity, readVoltage()));
   } // end current lp filter
 }
 
+
+///////////////
+// Functions //
+///////////////
+
+/******************************************************************************
+ * void runSpeedFilter()
+ *
+ * Calculates and generates output based on latest PWM input
+ * Runs at 20 Hz
+ ******************************************************************************/
+void runSpeedFilter() {
+  // Declare local variables
+  float rawvelocity;
+  dir_t direction;
+  float voltage = readVoltage();
+  float maxduty = min(maxDuty(voltage),MAX_DUTY);
+  float minduty = max(minDuty(voltage),0.0f);
+
+  // Reject signals that are way off (i.e. const. 0 V, const. +5 V, noise)
+  if ( pulsein >= INPUT_MIN && pulsein <= INPUT_MAX ) {
+    // Remove neutral PWM bias & clamp to [-HALF_RANGE, HALF_RANGE]
+    int16_t pw = constrain(pulsein - PWM_NEUTRAL, -HALF_RANGE, HALF_RANGE);
+
+    // Reject anything inside input deadzone
+    if ( pw > INPUT_DZ ) {
+    // Map valid PWM signals to (0.0 to 1.0]
+      rawvelocity = (float)(pw - INPUT_DZ)/(HALF_RANGE - INPUT_DZ)
+                    *(maxduty - minduty) + minduty;
+    } else if ( pw < -INPUT_DZ ) {
+      // Map valid PWM signals to [-1.0 to 0.0)
+      rawvelocity = (float)(pw + INPUT_DZ)/(HALF_RANGE - INPUT_DZ)
+                    *(maxduty - minduty) - minduty;
+    } else {
+      // Stop motor if input is within input deadzone
+      rawvelocity = 0.0f;
+    }
+  } else {
+    // Stop motor if input is invalid
+    rawvelocity = 0.0f;
+  }
+
+  // Filter velocity
+  velocity = constrain(speedfilter.step(rawvelocity), -maxduty, maxduty);
+
+  // Figure out the current direction of travel
+  if (velocity > minduty) {
+      direction = FORWARD;
+  } else if (velocity < -minduty) {
+      direction = REVERSE;
+  } else {
+      direction = NONE;
+  }
+
+  // Clear current filter if we change directions
+  if (direction != lastdir) {
+    currentfilter.clear();
+  }
+
+  // Current sensor resolution: ~65 mA
+  if (direction == FORWARD && currentfilter.getLastOutput() > I_LIMIT_OUT) {
+    limit = direction;
+    digitalWrite(LED, LOW);
+  } else if (direction == REVERSE && currentfilter.getLastOutput() > I_LIMIT_IN) {
+    limit = direction;
+    digitalWrite(LED, LOW);
+  } else if ( /*direction != NONE &&*/ limit != NONE && direction != limit ) {
+    // We're going the opposite direction from the limit, so clear limit
+    limit = NONE;
+    digitalWrite(LED, HIGH);
+  }
+
+  // Set output PWM timers
+  if ( direction == FORWARD && limit != FORWARD) {
+    OCR1A = abs(velocity)*MAX_COUNT;
+    OCR1B = 0;
+  } else if ( direction == REVERSE && limit != REVERSE) {
+    OCR1A = 0;
+    OCR1B = abs(velocity)*MAX_COUNT;
+  } else {
+    OCR1A = MAX_COUNT;
+    OCR1B = MAX_COUNT;
+  }
+
+  // Save last direction
+  lastdir = direction;
+}
+
+
+/******************************************************************************
+ * float readCurrent()
+ *
+ * Reads current consumption (Amperes)
+ ******************************************************************************/
+float readCurrent() {
+  // Reported voltage is 10x actual drop across the sense resistor
+  return (analogRead(CURRENT_IN)*V_IN)/(1023.0f*10.0f*R_SENSE);
+}
+
+/******************************************************************************
+ * float readVoltage()
+ *
+ * Reads input voltage (Volts)
+ ******************************************************************************/
+float readVoltage() {
+  return (analogRead(VOLTAGE_IN)*V_IN)/(1023.0f*V_SENSE_DIV);
+}
+
+/******************************************************************************
+ * float stallCurrent(float velocity, float voltage)
+ *
+ * Calculates stall current for given velocity and voltage (Amperes)
+ ******************************************************************************/
 float stallCurrent(float velocity, float voltage) {
   return (I_STALL_A0 + I_STALL_A1*voltage)*exp(I_STALL_B1*abs(velocity));
 }
 
+/******************************************************************************
+ * float maxDuty(float voltage)
+ *
+ * Calculates maximum safe output duty cycle for given voltage [-1,1]
+ ******************************************************************************/
 float maxDuty(float voltage) {
   return MAX_DUTY_A0 + MAX_DUTY_A1*voltage + MAX_DUTY_A2*voltage*voltage;
 }
 
+/******************************************************************************
+ * float minDuty(float voltage)
+ *
+ * Calculates minimum operational output duty cycle for given voltage [-1,1]
+ ******************************************************************************/
 float minDuty(float voltage) {
   return MIN_DUTY_A0 + MIN_DUTY_A1*voltage + MIN_DUTY_A2*voltage*voltage;
 }
 
+
+/******************************************************************************
+ * void initializePWMOutput()
+ *
+ * Sets registers to run timers at the proper frequency for the PWM output
+ ******************************************************************************/
 void initializePWMOutput() {
   // Stop interrupts while changing timer settings
   cli();
@@ -288,7 +287,12 @@ void initializePWMOutput() {
   sei();
 }
 
-// Set up external interrupt for PWM reader
+
+/******************************************************************************
+ * void initializePWMReader()
+ *
+ * Sets up external interrupt for PWM reader
+ ******************************************************************************/
 void initializePWMReader() {
   // Enable INT0
   bitSet(GIMSK, INT0);
@@ -297,18 +301,6 @@ void initializePWMReader() {
   bitSet  (MCUCR, ISC00);   // ISC00: 1
   bitClear(MCUCR, ISC01);   // ISC01: 0
 }
-
-// Read current (in amperes)
-float readCurrent() {
-  // Reported voltage is 10x actual drop across the sense resistor
-  return (analogRead(CURRENT_IN)*V_IN)/(1023.0f*10.0f*R_SENSE);
-}
-
-// Read input voltage (volts)
-float readVoltage() {
-  return (analogRead(VOLTAGE_IN)*V_IN)/(1023.0f*V_SENSE_DIV);
-}
-
 
 ////////////////////////////////
 // Interrupt Service Routines //
@@ -319,7 +311,11 @@ namespace {
   uint32_t inputpulsestart = 0xFFFF;
 }
 
-// Read PWM input
+/******************************************************************************
+ * SIGNAL(INT0_vect)
+ *
+ * Watches external interrupt to read PWM input
+ ******************************************************************************/
 SIGNAL(INT0_vect) {
   if (digitalRead(PWM_IN)) {
     // Record start of input pulse
