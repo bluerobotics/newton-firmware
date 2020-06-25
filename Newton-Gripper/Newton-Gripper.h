a2a0c902423145a4ee8046b249176e0e9ae94bf9
unknown <Brian Hoover>
2020-06-25 09:11:43 -0700

    Removed whitespace and changed fabs to abs per Jaxxzer's review

--- Newton-Gripper/Newton-Gripper.h
+++ Newton-Gripper/Newton-Gripper.h
@@ -24,7 +24,7 @@ uploaded via the Arduino 1.0+ software.
 -------------------------------
 The MIT License (MIT)
 
-Copyright (c) 2017 Blue Robotics Inc.
+Copyright (c) 2020 Blue Robotics Inc.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
